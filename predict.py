ef26865da0df94954dad1751573dd7a2ed1d40de
Saki <sakisku@users.noreply.github.com>
2026-02-05 15:25:20 +0100

    chore: parsing max_m

--- predict.py
+++ predict.py
@@ -8,9 +8,12 @@ if mileage < 0:
     exit(1)
     
 exec(open("model.txt").read())
-if 'teta0' not in locals() or 'teta1' not in locals() or 'max_m' not in locals():
+if 'teta0' not in locals() or 'teta1' not in locals():
     print("Model parameters are not properly defined.")
     exit(1)
 
-prediction = teta0 + (teta1 * mileage / max_m)
+if 'max_m' not in locals():
+    prediction = teta0 + (teta1 * mileage)
+elif 'max_m' in locals() and max_m > 0:
+    prediction = teta0 + (teta1 * mileage / max_m)
 print(f"Predicted price for a car with {int(mileage)} mileage is: {int(prediction)}")
